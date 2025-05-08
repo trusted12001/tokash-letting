26fc8d129dea068252655c3cc7e9ff16779789c0
Abdul <abdussalam.abdul@gmail.com>
2025-05-08 12:58:43 +0100

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon Start Here -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon-homlisti.svg') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/jquery-ui.css') }}">
     <!-- Bootstrap Css Start Here -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
